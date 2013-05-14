f01f0a3efa0b4878d12cb2ab67d3a2f898b4b620
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2013-05-13 21:32:52 -0400

    BROKEN: Work in progress
    This commit will not compile. Working on separating renaming logic from
    UI.

--- RenEx/RenExFileNameDescriptor.cs
+++ RenEx/RenExFileNameDescriptor.cs
@@ -8,16 +8,36 @@ namespace RenEx
 {
     /// <summary>
     /// FileNameDescriptor specialized for RenEx.
+    /// Stores the original file name and the renaming outcome.
     /// </summary>
     public class RenExFileNameDescriptor : FileNameDescriptor, IEquatable<FileNameDescriptor>
     {
+        #region Nested Types
+
+        [Flags]
+        public enum RenameState
+        {
+            None = 0,
+            Preview = 1,
+            Applied = 2,
+            Error = 4
+        }
+
+        #endregion
+        
         /// <summary>
         /// Constructor.
-        /// Initializes a new default instance.
+        /// Creates a copy of another FileNameDescriptor instance.
         /// </summary>
-        internal RenExFileNameDescriptor()
-            : base("AAA\\AAA.AAA", 0, null)
+        public RenExFileNameDescriptor(FileNameDescriptor descriptor)
         {
+            Directory = descriptor.Directory;
+            FileName = descriptor.FileName;
+            Extensions = descriptor.Extensions;
+
+            MarkForDelete = false;
+            IsApplied = false;
+            ErrorObject = null;
         }
 
         /// <summary>
@@ -30,6 +50,39 @@ namespace RenEx
         public RenExFileNameDescriptor(string path, int maxExt = 2, IFileExtValidator validator = null)
             : base(path, maxExt, validator)
         { }
+        
+        #region Transformed File Names
+
+        private String transformedDirectory;
+        private String transformedFileName;
+        private String transformedExtensions;
+
+        /// <summary>
+        /// Gets or sets the directory component after transformation.
+        /// </summary>
+        public String TransformedDirectory
+        {
+            get { return transformedDirectory ?? Directory; }
+            set { transformedDirectory = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the file name component after the transformation.
+        /// </summary>
+        public String TransformedFileName
+        {
+            get { return transformedFileName ?? FileName; }
+            set { transformedFileName = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the extension component after the transformation.
+        /// </summary>
+        public String TransformedExtensions
+        {
+            get { return transformedExtensions ?? Extensions; }
+            set { transformedExtensions = value; }
+        }
 
         /// <summary>
         /// Gets or sets a value indicating whether the file is marked for deletion.
@@ -37,10 +90,12 @@ namespace RenEx
         public Boolean MarkForDelete { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the renaming rule
-        /// has been applied to the descriptor.
+        /// Gets the file path after transformation.
         /// </summary>
-        public Boolean IsApplied { get; set; }
+        public String TransformedFilePath
+        {get { return TransformedDirectory + "\\" + TransformedFileName + TransformedExtensions; }}
+
+        #endregion
 
         /// <summary>
         /// Gets or sets the Exception object associated to the transforming process.
---
+++ RenEx/RenamingSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RenEx
+{
+    /// <summary>
+    /// Renaming session with all the variables contained in it.
+    /// </summary>
+    public class RenamingSession
+    {
+         
+    }
+}
